8eb9b5a3026faca901f5860a16ebc9a241de87f1
Andrés Alegría <andresalegria@live.com>
2025-10-02 23:07:17 +0200

    Content update

--- src/config.js
+++ src/config.js
@@ -33,6 +33,8 @@ export default {
   ],
   alignment: 'left',
   footer: 'Cartography by Andrés Alegría | Edited by Kartik Chandramouli',
+ 
+ 
   chapters: [
 
     // chapter 1
@@ -40,103 +42,29 @@ export default {
       id: 'chapter 1',
       alignment: 'left',
       hidden: false,
-      title: 'Weaving a dangerous web',
-      description: "New oil pipelines are putting some of the planet’s last biodiversity strongholds — and many Indigenous lands — at risk.",
+      title: ' ',
+      description: "The construction industry is Kerala’s largest consumer of river sand. After the ban on river sand mining, miners turned to the hills of the Western Ghats, crushing quarried hard rock to produce manufactured sand, also known as <b>m-sand</b>.",
        legend: [
         {
-          title: 'Global network of oil/gas pipelines',
+          title: 'XXX',
           fromLayer: 'Pipelines existing',
         },
       ],
-      sources: 'Global Energy Monitor',
       location: {
-        center: [31.5, 1.25],
-        zoom: 1.8,
+        center: [75.0, 13.0],
+        zoom: 4.75,
         pitch: 0.0,
         bearing: 0,
       },
-      images: [
-        {
-          src: 'Photo1.jpg',
-          position: 'top',
-          title: 'Oil/Gas Pipeline',
-          author: 'R. Naik',
-        },
-      ],
       mapAnimation: 'easeTo',
       rotateAnimation: false,
-      callback: '',
       onChapterEnter: [
         {
           layer: 'Pipeline_icons',
           opacity: 0,
         },
-        {
-          layer: 'Satellite_2025',
-          opacity: 0,
-        },
-                  {
-          layer: 'Satellite_2023',
-          opacity: 0,
-        },
-        {
-          layer: 'wdpas-2025-Murchison',
-          opacity: 0,
-        },
-               {
-          layer: 'wdpas-2025-Murchison fill',
-          opacity: 0,
-        },
-        {
-          layer: 'GSN-climate-stabilization',
-          opacity: 0,
-        },
-        {
-          layer: 'GSN-potential-corridors',
-          opacity: 0,
-        },
-        {
-          layer: 'wdpas-2025',
-          opacity: 0,
-        },
-        {
-          layer: 'GSN-intact-wilderness-areas',
-          opacity: 0,
-        },
-        {
-          layer: 'GSN-high-biodiversity',
-          opacity: 0,
-        },
-        {
-          layer: 'GSN-large-mammals',
-          opacity: 0,
-        },
-          {
-          layer: 'admin-0-boundary-disputed',
-          opacity: 0,
-        },  
-          {
-          layer: 'admin-0-boundary-a',
-          opacity: 0,
-        },  
-          {
-          layer: 'admin-0-boundary-b',
-          opacity: 0,
-        },  
-         {
-          layer: 'wdpas-2025 color threatend',
-          opacity: 0,
-        },
       ],
         onChapterExit: [
-          {
-          layer: 'admin-0-boundary-a',
-          opacity: 1,
-        },  
-          {
-          layer: 'admin-0-boundary-b',
-          opacity: 1,
-        },  
       ],
     
     },
@@ -147,8 +75,8 @@ export default {
       id: 'chapter 2',
       alignment: 'left',
       hidden: false,
-      title: 'EACOP',
-      description: "The East African Crude Oil Pipeline (EACOP) will run 1,443 kilometers (897 miles) from Uganda's newly developed oil fields on Lake Albert to the Tanzanian port of Tanga.",
+      title: ' ',
+      description: "72 legal quarries lie within 10 km of 20 protected areas in the Western Ghats, one of the world’s most biodiverse regions. Most expanded after the ban.",
              legend: [
         {
           title: 'Existing pipelines',
@@ -159,79 +87,22 @@ export default {
           fromLayer: 'Pipelines under construction',
         },
          ],
-      location: {
-        center: [36.0, -2.8],
-        zoom: 4.2,
-        pitch: 0.0,
-        bearing: 0,
+      sources: 'Ashok L, et al. (2025)',
+ location: {
+        center: [77.5, 11.5],
+        zoom: 6.25,
+        pitch: 62.0,
+        bearing: 60,
       },
-     
       mapAnimation: 'easeTo',
       rotateAnimation: false,
-      callback: '',
       onChapterEnter: [
                   {
           layer: 'admin-0-boundary-a',
           opacity: 1,
         },  
-          {
-          layer: 'admin-0-boundary-b',
-          opacity: 1,
-        },  
       ],
         onChapterExit: [
-        {
-          layer: 'Pipeline_icons',
-          opacity: 0,
-        },
-        {
-          layer: 'Satellite_2025',
-          opacity: 0,
-        },
-                  {
-          layer: 'Satellite_2023',
-          opacity: 0,
-        },
-        {
-          layer: 'wdpas-2025-Murchison',
-          opacity: 0,
-        },
-                     {
-          layer: 'wdpas-2025-Murchison fill',
-          opacity: 0,
-        },
-        {
-          layer: 'GSN-climate-stabilization',
-          opacity: 0,
-        },
-        {
-          layer: 'GSN-potential-corridors',
-          opacity: 0,
-        },
-        {
-          layer: 'wdpas-2025',
-          opacity: 0,
-        },
-        {
-          layer: 'GSN-intact-wilderness-areas',
-          opacity: 0,
-        },
-        {
-          layer: 'GSN-high-biodiversity',
-          opacity: 0,
-        },
-        {
-          layer: 'GSN-large-mammals',
-          opacity: 0,
-        },
-          {
-          layer: 'admin-0-boundary-disputed',
-          opacity: 0,
-        },  
-         {
-          layer: 'wdpas-2025 color threatend',
-          opacity: 0,
-        },          
       ],
     
     },
@@ -242,56 +113,29 @@ export default {
       id: 'chapter 3',
       alignment: 'left',
       hidden: false,
-      title: 'Where pipelines meet critical ecosystems',
-      description: "Construction and operation of oil infrastructure risks irreversible damage to critical wildlife habitat, water bodies and farmland.The pipeline and its 2-km (1.2-mi) buffer zone are impacting more than 100,000 people and dozens of formal protected areas, starting with the wellheads and feeder lines in and around Murchison Falls National Park.",
+      title: ' ',
+      description: "Malabar Wildlife Sanctuary, 2015 (before the ban).",
              legend: [
         {
           title: 'Critical areas based on the Global Safety Net Initiative (GSN)',
           color: '#006a54',
         },
          ],
-      sources: 'GSN',
-      location: {
-        center: [31.506, -1.5],
-        zoom: 5.6,
-        pitch: 0.0,
-        bearing: 0,
+        location: {
+        center: [75.90, 11.60],
+        zoom: 12.5,
+        pitch: 62.0,
+        bearing: 60,
       },
-     
       mapAnimation: 'easeTo',
       rotateAnimation: false,
-      callback: '',
       onChapterEnter: [
         {
           layer: 'GSN-climate-stabilization',
           opacity: 1,
         },
-        {
-          layer: 'GSN-potential-corridors',
-          opacity: 1,
-        },
-        {
-          layer: 'wdpas-2025',
-          opacity: 1,
-        },
-        {
-          layer: 'GSN-intact-wilderness-areas',
-          opacity: 1,
-        },
-        {
-          layer: 'GSN-high-biodiversity',
-          opacity: 1,
-        },
-        {
-          layer: 'GSN-large-mammals',
-          opacity: 1,
-        },
       ],
         onChapterExit: [
-                  {
-          layer: 'wdpas-2025 color threatend',
-          opacity: 0,
-        },
       ],
     
     },
@@ -302,80 +146,29 @@ export default {
       id: 'chapter 4',
       alignment: 'left',
       hidden: false,
-      title: 'Protected areas under threat',
-      description: "By July 2025, nine wellpads and 38 km (23.6 mi) of roads had been constructed in the park. Campaigners have reported disturbances to elephants and other wildlife in the park and raised fears over a pipeline crossing the Nile River in a Ramsar-listed wetland that is a refuge for endangered species like the shoebill stork (<i>Balaeniceps rex</i>).",
+      title: ' ',
+      description: "Malabar Wildlife Sanctuary, 2016 (after the ban).",
              legend: [
         {
           title: 'Protected areas affected by EACOP',
           color: '#bdeaaf',
         },
          ],
-       sources: 'Earth Insight',
-     images: [
-        {
-          src: 'photo2.jpg', 
-          position: 'top',
-          title: 'Shoebill storks',
-          author: 'K. Marino',
-        },
-      ],  
       location: {
-        center: [31.506, -1.5],
-        zoom: 5.6,
-        pitch: 0.0,
-        bearing: 0,
+        center: [75.90, 11.60],
+        zoom: 12.5,
+        pitch: 62.0,
+        bearing: 60,
       },
-     
       mapAnimation: 'easeTo',
       rotateAnimation: false,
-      callback: '',
       onChapterEnter: [
          {
           layer: 'GSN-climate-stabilization',
           opacity: 0,
         },
-        {
-          layer: 'GSN-potential-corridors',
-          opacity: 0,
-        },
-        {
-          layer: 'wdpas-2025',
-          opacity: 0,
-        },
-        {
-          layer: 'GSN-intact-wilderness-areas',
-          opacity: 0,
-        },
-        {
-          layer: 'GSN-high-biodiversity',
-          opacity: 0,
-        },
-        {
-          layer: 'GSN-large-mammals',
-          opacity: 0,
-        },
       ],
         onChapterExit: [
-            {
-          layer: 'wdpas-2025 color threatend',
-          opacity: 1,
-        },
-                         {
-          layer: 'wdpas-2025-Murchison',
-          opacity: 0,
-        },
-                     {
-          layer: 'wdpas-2025-Murchison fill',
-          opacity: 0,
-        },
-                {
-          layer: 'Satellite_2025',
-          opacity: 0,
-        },
-                  {
-          layer: 'Satellite_2023',
-          opacity: 0,
-        },
       ],
     
     },
@@ -386,53 +179,29 @@ export default {
       id: 'chapter 5',
       alignment: 'left',
       hidden: false,
-      title: 'National treasure',
-      description: "Every year, more than 100,000 tourists visit Murchison Falls National Park, Uganda's oldest and largest protected area, to see elephants, lions, giraffes and numerous endemic species, generating income for the government and nearby communities.",
+      title: ' ',
+      description: "Malabar Wildlife Sanctuary has the highest number of quarries —15— within its 10 km buffer.",
              legend: [
         {
           title: 'Murchison Falls National Park',
-     // fromLayer: 'wdpas-2025-Murchison',
             border: '#c3a7c5',
         },
          ],
-     sources: 'Satellite image from Copernicus',       
-      location: {
-        center: [31.45, 2.175],
-        zoom: 10.75,
-        pitch: 0.0,
-        bearing: 0,
+     location: {
+        center: [75.90, 11.60],
+        zoom: 12.5,
+        pitch: 62.0,
+        bearing: 60,
       },
-     
       mapAnimation: 'easeTo',
       rotateAnimation: false,
-      callback: '',
       onChapterEnter: [
                           {
           layer: 'wdpas-2025 color',
           opacity: 0,
         },
-                 {
-          layer: 'wdpas-2025-Murchison',
-          opacity: 1,
-        },
-                     {
-          layer: 'wdpas-2025-Murchison fill',
-          opacity: 0.2,
-        },
-                  {
-          layer: 'Satellite_2023',
-          opacity: 1,
-        },
       ],
         onChapterExit: [
-                 {
-          layer: 'Satellite_2025',
-          opacity: 1,
-        },
-             {
-          layer: 'Pipeline_icons',
-          opacity: 0,
-        },
       ],
     
     },
@@ -443,52 +212,29 @@ export default {
       id: 'chapter 6',
       alignment: 'left',
       hidden: false,
-      title: 'Worrying footprint',
-      description: "Unnecessary risk.",
+      title: ' ',
+      description: "The Silent Valley National Park, home to the endangered lion-tailed macaque, has three quarries just four kilometres from its boundary.",
            legend: [
         {
           title: 'Tilenga Feeder Pipeline Trajectory',
           icon: 'pipeline',
         },
          ],
-      sources: 'Satellite image from Copernicus',
       location: {
-        center: [31.45, 2.175],
-        zoom: 10.75,
-        pitch: 0.0,
-        bearing: 0,
+        center: [76.45, 11.35],
+        zoom: 12.5,
+        pitch: 62.0,
+        bearing: 60,
       },
-     
       mapAnimation: 'easeTo',
       rotateAnimation: false,
-      callback: '',
       onChapterEnter: [
                           {
           layer: 'wdpas-2025 color',
           opacity: 0,
         },
-                 {
-          layer: 'wdpas-2025-Murchison',
-          opacity: 1,
-        },
-                     {
-          layer: 'wdpas-2025-Murchison fill',
-          opacity: 0.2,
-        },
-                  {
-          layer: 'Satellite_2023',
-          opacity: 0,
-        },
       ],
         onChapterExit: [
-                      {
-          layer: 'Pipeline_icons',
-          opacity: 1,
-        },
-                 {
-          layer: 'Satellite_2025',
-          opacity: 1,
-        },
       ],
     
     },
@@ -501,69 +247,19 @@ export default {
       id: 'chapter 7',
       alignment: 'left',
       hidden: false,
-      title: 'Unnecessary risk',
-      description: "This East African pipeline dispossesses thousands of people, threatens vital water supplies and puts sensitive ecosystems at risk — its carbon footprint will be 25 times the combined annual emissions of Tanzania and Uganda.",
+      title: ' ',
+      description: "Some protected areas, such as the Aralam Wildlife Sanctuary, have quarries one kilometer from their boundary. Blasting increases noise and dust pollution, posing risks to both wildlife and humans.",
       location: {
-        center: [31.506, -1.5],
-        zoom: 5.6,
-        pitch: 0.0,
-        bearing: 0,
+        center: [75.84, 11.94],
+        zoom: 12.5,
+        pitch: 62.0,
+        bearing: 60,
       },
-     
       mapAnimation: 'easeTo',
       rotateAnimation: false,
-      callback: '',
       onChapterEnter: [
         {
           layer: 'Pipeline_icons',
-          opacity: 0,
-        },
-        {
-          layer: 'Satellite_2025',
-          opacity: 0,
-        },
-                  {
-          layer: 'Satellite_2023',
-          opacity: 0,
-        },
-        {
-          layer: 'wdpas-2025-Murchison',
-          opacity: 0,
-        },
-               {
-          layer: 'wdpas-2025-Murchison fill',
-          opacity: 0,
-        },
-        {
-          layer: 'GSN-climate-stabilization',
-          opacity: 0,
-        },
-        {
-          layer: 'GSN-potential-corridors',
-          opacity: 0,
-        },
-        {
-          layer: 'wdpas-2025',
-          opacity: 0,
-        },
-        {
-          layer: 'GSN-intact-wilderness-areas',
-          opacity: 0,
-        },
-        {
-          layer: 'GSN-high-biodiversity',
-          opacity: 0,
-        },
-        {
-          layer: 'GSN-large-mammals',
-          opacity: 0,
-        },
-          {
-          layer: 'admin-0-boundary-disputed',
-          opacity: 0,
-        },  
-         {
-          layer: 'wdpas-2025 color threatend',
           opacity: 0,
         },
       ],
@@ -571,6 +267,110 @@ export default {
       ],
     },
        
+ // chapter 8
+    {
+      id: 'chapter 8',
+      alignment: 'left',
+      hidden: false,
+      title: ' ',
+      description: "2011: Quarry footprint.",
+     location: {
+        center: [75.84, 11.94],
+        zoom: 12.5,
+        pitch: 62.0,
+        bearing: 60,
+      },
+      mapAnimation: 'easeTo',
+      rotateAnimation: false,
+      onChapterEnter: [
+        {
+          layer: 'Pipeline_icons',
+          opacity: 0,
+        },
+      ],
+        onChapterExit: [
+      ],
+    },
+
+ 
+ // chapter 9
+    {
+      id: 'chapter 9',
+      alignment: 'left',
+      hidden: false,
+      title: ' ',
+      description: "2016: After the ban, sites expanded.",
+     location: {
+        center: [75.84, 11.94],
+        zoom: 12.5,
+        pitch: 62.0,
+        bearing: 60,
+      },
+      mapAnimation: 'easeTo',
+      rotateAnimation: false,
+      onChapterEnter: [
+        {
+          layer: 'Pipeline_icons',
+          opacity: 0,
+        },
+      ],
+        onChapterExit: [
+      ],
+    },
+
+ 
+ // chapter 10
+    {
+      id: 'chapter 10',
+      alignment: 'left',
+      hidden: false,
+      title: ' ',
+      description: "2021: 90% of quarries had grown by more than 50%.",
+      location: {
+        center: [75.84, 11.94],
+        zoom: 12.5,
+        pitch: 62.0,
+        bearing: 60,
+      },
+      mapAnimation: 'easeTo',
+      rotateAnimation: false,
+      onChapterEnter: [
+        {
+          layer: 'Pipeline_icons',
+          opacity: 0,
+        },
+      ],
+        onChapterExit: [
+      ],
+    },
+
+
+ 
+ // chapter 11
+    {
+      id: 'chapter 11',
+      alignment: 'left',
+      hidden: false,
+      title: ' ',
+      description: "This study doesn’t factor in the other illegal quarrying in Kerala’s Western Ghats.",
+     location: {
+        center: [76.0, 10.25],
+        zoom: 7.0,
+        pitch: 5.0,
+        bearing: 0,
+      },
+      mapAnimation: 'easeTo',
+      rotateAnimation: false,
+      onChapterEnter: [
+        {
+          layer: 'Pipeline_icons',
+          opacity: 0,
+        },
+      ],
+        onChapterExit: [
+      ],
+    },
+
        
    
 
